4ba936ea20514aa26f6a4e14e10ab6f4dd0de31a
Oscar Hermansson <oscar.hermansson@learnet.se>
2026-01-09 10:38:28 +0100

    fixat med Door Animation och TriggerEvent

--- Assets/Scripts/DoorFunction.cs
+++ Assets/Scripts/DoorFunction.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class DoorFunction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator animator;
+    public bool DoorOpen = false;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("OnTriggerEnter: " + other.name + ", Tag: " + other.tag);
+        if (other.CompareTag("Player") && !DoorOpen)
+        {
+            Debug.Log("Player entered and door is closed. Opening door.");
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit: " + other.name + ", Tag: " + other.tag);
+        if (other.CompareTag("Player") && DoorOpen)
+        {
+            Debug.Log("Player exited and door is open. Closing door.");
+            CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        animator.SetBool("DoorOpen", true);
+        DoorOpen = true;
+    }
+
+    private void CloseDoor()
+    {
+        animator.SetBool("DoorOpen", false);
+        DoorOpen = false;
     }
 }
