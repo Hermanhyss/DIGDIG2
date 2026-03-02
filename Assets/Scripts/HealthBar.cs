0ce77f093e8578b25cf77afd12ddc0f578a05bf3
Hermanhyss <oshermansson06@gmail.com>
2026-03-02 17:21:33 +0100

    Fixat med HpBar till Player

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,52 +4,31 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [Header("Health Settings")]
-    [SerializeField] private float health;
-    [SerializeField] private float maxHealth;
     [SerializeField] private float width;
     [SerializeField] private float height;
 
     [Header("UI Reference")]
     [SerializeField]
     private RectTransform healthBar;
-    public float Health
+
+    private PlayerController playerController;
+
+    private void Start()
     {
-        get => health;
-        set
-        {
-            health = Mathf.Clamp(value, 0, maxHealth);
-            UpdateBar();
-        }
-    }
-    public float MaxHealth
-    {
-        get => maxHealth;
-        set
-        {
-            maxHealth = Mathf.Max(1, value);
-            health = Mathf.Clamp(health, 0, maxHealth);
-            UpdateBar();
-        }
-    }
-    private void OnValidate()
-    {
-        Health = health;
-        MaxHealth = maxHealth;
+        playerController = FindObjectOfType<PlayerController>();
         UpdateBar();
     }
-    public void SetMaxHealth(float value)
+
+    private void Update()
     {
-        MaxHealth = value;
+        UpdateBar();
     }
-    public void SetHealth(float value)
-    {
-        Health = value;
-    }
+
     private void UpdateBar()
     {
-        if (healthBar != null && maxHealth > 0)
+        if (playerController != null && healthBar != null && playerController.maxHealth > 0)
         {
-            float newWidth = (health / maxHealth) * width;
+            float newWidth = (playerController.CurrentHealth / playerController.maxHealth) * width;
             healthBar.sizeDelta = new Vector2(newWidth, height);
         }
     }
