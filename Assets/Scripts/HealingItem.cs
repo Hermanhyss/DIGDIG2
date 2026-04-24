555b3cd194401fba30fe9189fe7dafc8df0c05fa
UndeadElite <145747901+TheUndeadElite@users.noreply.github.com>
2026-04-24 11:31:30 +0200

    Healing item, and new E key icon

--- Assets/Scripts/HealingItem.cs
+++ Assets/Scripts/HealingItem.cs
@@ -1,28 +1,36 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class HealingItem : MonoBehaviour
 {
     Material healingMaterial;
-    int healBoolValue = 0;
+    GameObject HealingModel;
+    BoxCollider boxCollider;
+    PlayerController playerController;
 
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            playerController = other.GetComponent<PlayerController>();
             SkinnedMeshRenderer renderer = other.GetComponentInChildren<SkinnedMeshRenderer>();
 
             healingMaterial = renderer.materials[1];
-            Debug.Log("material get");
-            StartCoroutine(SetBoolForTime(healingMaterial, 1, 2f));
-            Debug.Log("Player has interacted with healing");
+            StartCoroutine(SetBoolForTime(healingMaterial, 1f));
+            playerController.currentHealth += 30;
         }
     }
 
-    IEnumerator SetBoolForTime(Material healingMaterial, int healBoolValue, float time)
+    IEnumerator SetBoolForTime(Material healingMaterial, float time)
     {
-        Debug.Log("coroutine started");
+        var component = GetComponentInChildren<MeshRenderer>();
+        Destroy(component.gameObject);
         healingMaterial.SetInt("_PickedUp", 1);
         yield return new WaitForSeconds(time);
         healingMaterial.SetInt("_PickedUp", 0);
