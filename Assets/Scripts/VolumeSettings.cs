b0ac862b514ed0ca8e1b9ee4d042577729d38139
Alvito0077 <Alva.Alexandra.B@gmail.com>
2026-05-08 11:51:26 +0200

    Håller på med sliders och mixer

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -1,16 +1,45 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+    [SerializeField] private Slider UISlider;
+    [SerializeField] private Slider masterSlider;
+
+    private void Start()
     {
-        
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolume()
     {
-        
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume); //sparar inställningar för musiken
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume"); //laddar inställningar för musiken
+
+        SetMusicVolume(); 
     }
 }
